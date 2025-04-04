166b8c24760056687ff5861175ac6af544573da8
NandiniB7 <nandinibharadwaj.21@stvincentngp.edu.in>
2025-04-04 19:37:25 +0530

    Add files via upload

---
+++ employee_final/database.py
@@ -0,0 +1,11 @@
+import pyodbc
+
+def get_db_connection():
+    conn = pyodbc.connect(
+        "DRIVER={SQL Server};"
+        "SERVER=NICESS-LP236;"
+        "DATABASE=EmployeeManagement;"
+        "UID=sa;"
+        "PWD=training@123;"
+    )
+    return conn
